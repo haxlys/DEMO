000472b7830b7d2f1737fe6fdfc5d08c4fe4f260
haxlys <haxlys@gmail.com>
2017-01-08 13:18:41 +0900

    시스템 에러 별도 파일로 분리

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -2,23 +2,28 @@ package com.company;
 
 import org.apache.log4j.Logger;
 
-/**
- * Created by mac on 2016. 12. 12..
- */
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class Test {
 
     public static void main(String[] args) throws Exception {
         Logger logger = Logger.getRootLogger();
+        try{
+            System.setErr(new PrintStream(new FileOutputStream(System.getProperty("user.home")+"/LOG/rocketpunch/error.log")));
 
-        long start = System.currentTimeMillis();
+            long start = System.currentTimeMillis();
 
-        //ThreadWork.execute(); // 서버가 요청을 받아주지 못하고 503에러 뱉음
-        String targetUrl = "https://www.rocketpunch.com/jobs?job=%EA%B0%9C%EB%B0%9C%EC%9E%90";
-        String baseUrl = "https://www.rocketpunch.com";
-        String pageParamName = "page";
-        URLConnection u = new URLConnection(targetUrl, baseUrl, pageParamName);
-        u.execute();
+            //ThreadWork.execute(); // 서버가 요청을 받아주지 못하고 503에러 뱉음
+            String targetUrl = "https://www.rocketpunch.com/jobs?job=%EA%B0%9C%EB%B0%9C%EC%9E%90";
+            String baseUrl = "https://www.rocketpunch.com";
+            String pageParamName = "page";
+            URLConnection u = new URLConnection(targetUrl, baseUrl, pageParamName);
+            u.execute();
 
-        logger.info( "실행 시간 : " + ( System.currentTimeMillis() - start )/1000.0 );
+            logger.info( "실행 시간 : " + ( System.currentTimeMillis() - start )/1000.0 );
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
